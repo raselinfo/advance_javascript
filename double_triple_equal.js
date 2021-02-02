cae682c4ca690a8e1ef2be95094f95907ce0f75e
raselofficial52 <raselofficial52@gmail.com>
2021-02-03 00:40:01 +0600

    double triple equal

---
+++ double_triple_equal.js
@@ -0,0 +1,28 @@
+const first = 2;
+const second = 3;
+
+if (first == second) {
+    console.log(`Condition is true`);
+} else {
+    console.log(`Condition is false`);
+}
+
+
+const first = 2;
+const second = "2";
+
+if (first == second) {
+    console.log(`Condition is true`);
+} else {
+    console.log(`Condition is false`);
+}
+
+
+const first = 2;
+const second = "2";
+
+if (first === second) {
+    console.log(`Condition is true`);
+} else {
+    console.log(`Condition is false`);
+}
