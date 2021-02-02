ff8425a1c99ac146c1bf54528b1b041c63a74946
raselofficial52 <raselofficial52@gmail.com>
2021-02-03 00:26:49 +0600

    undefiend vs null

---
+++ undefiend-VS-null.js
@@ -0,0 +1,25 @@
+let bird;
+console.log(`${bird}`);
+
+
+function add(num1,num2){
+    console.log(`${num1+num2}`);    
+    return 
+}
+
+
+function add(num1,num2){
+    console.log(num2);    
+}
+
+
+const student={
+    name:"Rasel Hossain",
+    phone:523535,
+}
+console.log(student.class)
+
+
+let fun=undefined;
+console.log(`${fun}`);
+
