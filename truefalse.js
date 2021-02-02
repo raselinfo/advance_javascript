8105eba476617c4a14ab3da0d0f9b1f1ea322d4c
raselofficial52 <raselofficial52@gmail.com>
2021-02-03 00:09:52 +0600

    truthy and falsy value

---
+++ truefalse.js
@@ -0,0 +1,50 @@
+//*** Truthy value
+// const value = " "
+// const value = true;
+// const value = "false";
+// const value = 1;
+// const value = {};
+// const value = [];
+// const value = 20;
+// const value = 20;
+// const value = -30;
+// const value = "0";
+// const value = new Date();
+// const value = 12n;
+// const value = -6.4;
+// const value = 6.4;
+// const value = Infinity;
+// const value = -Infinity;
+// const value = 1 / 0;
+// const value = 1 / 0;
+// const value = !false;
+// const value = !NaN;
+// const value = !undefined;
+// const value = !null;
+// const value = !!null;
+// const value = !!undefined;
+// const value = !10 === !10;
+// const value = !false === !false;
+// const value = !true === !true;
+// const value = !true === !!true;
+
+
+//*** Falsy Value
+    // const value = ""
+    // const value = false;
+    // const value = 0;
+    // const value = null;
+    // const value = -0;
+    // const value = NaN;
+    // const value = undefined;
+    // const value = !true;
+    // const value = !!!undefined;
+    // const value = !!!null;
+
+
+if (value) {
+    console.log(`Truthy Value`);
+} else {
+    console.log(`Falsy Value`);
+
+}
