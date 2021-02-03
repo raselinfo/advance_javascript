25acebfceb6945829dae011e50f3f5e37ab281d2
raselofficial52 <raselofficial52@gmail.com>
2021-02-03 14:23:59 +0600

    array split

---
+++ array-split.js
@@ -0,0 +1,15 @@
+const numbers=[1,2,3,4,5,6,7,8,9]
+const part=numbers.slice(2,5);
+// const removed=numbers.splice(2,5,"Rasel","Rabina")
+// console.log(part);
+// console.log(removed);
+// console.log(numbers);
+// console.log(part);
+
+// const together=numbers.join(" | ");
+const together=numbers.join(" ami ");
+
+console.log(together);
+
+
+
